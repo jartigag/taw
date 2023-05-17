7add5ebbb4029f1d61c27e8f465d0c924fcc263b
jartigag <jartigag@pm.me>
2023-05-17 20:19:27 +0200

    fixes in graph

--- TAW.py
+++ TAW.py
@@ -21,6 +21,10 @@
 #
 #  $ pipenv run ./TAW.py -2,-1,0
 #
+#  Print hours per project worked this year:
+#
+#  $ pipenv run ./TAW.py $(seq -s, 1 $(date +%U))
+#
 # └=========================================================┘
 #
 # More info: https://github.com/jartigag/taw
@@ -71,8 +75,8 @@ def convert_hhmm_to_decimal_hour(hhmm_string):
 def working_days_of_a_specific_week(week_number=0):
     if week_number<=0:
     # so week_number is relative to today:
-        today   = datetime.today().date()
-        ref_day = today
+        today               = datetime.today().date()
+        ref_day             = today
     else:
     # so week_number is relative to the actual year:
         first_day_this_year = datetime.today().date().replace(month=1, day=1)
@@ -100,7 +104,6 @@ def ascii_bar(value, max_value, max_bar_length):
 
 def init_nested_dict(dirs):
     hours_per_project = {}
-
     for dir in dirs:
         if Path(dir+"/").is_dir():
             for subdir in Path(dir).iterdir():
@@ -110,38 +113,45 @@ def init_nested_dict(dirs):
         else:
             print(f'[!] Folder "{dir}" not found')
             sys.exit(-1)
-
     # Sort dict by code if project code between parenthesis, else sort by project name:
     hours_per_project = {k: hours_per_project[k] for k in sorted(
                             hours_per_project.keys(),
                             key=lambda x: x[x.find("(")+1:x.find(")")] if x.find('(')!=x.find(')')!=-1 else x
                         )}
-
     return hours_per_project
 
 def load_colors_config(config_file):
     colors = {}
-
     if Path(config_file).is_file():
         colors_config = json.load(open(config_file))
     else:
         colors_config = {'calendarSources': ''}
-
     def replace_all(text, dic):
         for i, j in dic.items():
             text = text.replace(i, j)
         return text
-
     strings_to_remove = list(set([x for sublist in TAW_DIRS for x in sublist.split('/')])) + ["/"]
-
     for calendarSources in colors_config['calendarSources']:
         colors[replace_all(calendarSources['directory'], {k:"" for k in strings_to_remove})] = calendarSources['color']
-
     return colors
+
+def sort_projects(projects, config_file):
+    # Place 'VACACIONES' project at the end of the list and sort it:
+    #TODO: configurable order
+    projects = list(hours_per_project.keys())
+    projects.remove('VACACIONES (23-0004)')
+    projects = sorted(projects,
+                key=lambda x: x[x.find("(")+1:x.find(")")] if x.find('(')!=x.find(')')!=-1 else x,
+                reverse=True) + [('VACACIONES (23-0004)')]
+    return projects
+
 
 if __name__ == "__main__":
 
-    # Get the command line arguments:
+    # Create nested dictionary to store worked hours:
+    hours_per_project = init_nested_dict(TAW_DIRS)
+
+    # Get command line arguments:
     args = sys.argv[1:]
     if not args:
         weeks = [0]
@@ -149,11 +159,8 @@ if __name__ == "__main__":
         try:
             weeks = sorted([int(x) for x in args[0].split(',')])
         except ValueError:
-            print("[!] The provided week numbers must be integers separated by commas")
+            print("[!] Provided week numbers must be integers separated by commas")
             sys.exit(-1)
-
-    # Create the nested dictionary to store the worked hours:
-    hours_per_project = init_nested_dict(TAW_DIRS)
 
     #TODO: simplify (2 `for week in weeks`? `contemplated_dates` and `contemplated_weeks?`)
     # Set the dates on which worked hours will be added:
@@ -176,49 +183,49 @@ if __name__ == "__main__":
                     for filename in Path(dir+"/"+project).iterdir():
                         if filename.is_file() and filename.suffix==".md":
                             try:
-                                date_in_filename = datetime.strptime(filename.name.split()[0], "%Y-%m-%d").date()
-                            except ValueError:
-                                continue
-                            if date_in_filename in contemplated_dates:
+                                # date_in_file_name = datetime.strptime(filename.name.split()[0], "%Y-%m-%d").date()
                                 data = yaml.safe_load(quote_hours_in_yaml_header(filename))
                                 date = data['date']
                                 if date in contemplated_dates:
                                     duration = convert_hhmm_to_decimal_hour(data['endTime']) - convert_hhmm_to_decimal_hour(data['startTime'])
                                     hours_per_project[project][date]['hours'] += duration
                                     hours_per_project[project][date]['notes'].add(data['title'])
+                            except ValueError:
+                                continue
 
-        # Convert the dictionary into a list of rows for the table:
+        # Convert the dictionary into a table rows list:
         table_rows = []
         if len(hours_per_project)>0:
-            max_num_chars_project_name = len( max(hours_per_project.keys(), key=len) )
+            max_num_chars_project_name = len(max(hours_per_project.keys(), key=len))
             num_chars_hours            = len("00.0 h (00.0 %)")
             for project in hours_per_project:
-                hours_row = [f"┌-{project}{' '*( max(max_num_chars_project_name, num_chars_hours)-len(project) )}-┐"]
+                hours_row = [f"┌-{project}{' '*(max(max_num_chars_project_name, num_chars_hours)-len(project))}-┐"]
                 notes_row = [""]
                 for days in contemplated_weeks[week]:
                     for day in days:
-                        if hours_per_project[project][date]['hours']>0:
+                        if hours_per_project[project][day]['hours']>0:
                             hours_row.append(hours_per_project[project][day]['hours'])
                             unified_notes = ". ".join(hours_per_project[project][day]['notes'])
-                            if len(unified_notes)>30:
+                            if len(unified_notes) > 30:
                                 notes_row.append("\n".join(hours_per_project[project][day]['notes']))
                             else:
                                 notes_row.append(unified_notes)
                         else:
                             hours_row.append("-")
                             notes_row.append("")
-                if any(h!='-' for h in hours_row[1:]): # excluding the first element of the row, which is the project name
+                if any(h!='-' for h in hours_row[1:]):  # excluding the first element of the row, which is the project name
                     table_rows.append(hours_row)
                     total_hours_row     = sum(filter(lambda x: x != "-", hours_row[1:]))
                     total_hours_row_str = "{:04.1f}".format(total_hours_row)
+                    # FIXME: set n_week correctly
                     percent_str         = "{:04.1f}".format(total_hours_row/TOTAL_WORKING_HOURS_EACH_WEEK[n_week]*100)
-                    notes_row[0]        = "||" + "".join(ascii_bar(
-                                                         total_hours_row,
-                                                         TOTAL_WORKING_HOURS_EACH_WEEK[n_week],
-                                                         max(max_num_chars_project_name, num_chars_hours)
-                                          )) + "||"
+                    notes_row[0]        = "||"+"".join(ascii_bar(
+                                                total_hours_row,
+                                                TOTAL_WORKING_HOURS_EACH_WEEK[n_week],
+                                                max(max_num_chars_project_name, num_chars_hours)
+                                            ))+"||"
                     table_rows.append(notes_row)
-                    table_rows.append([f"└-{total_hours_row_str} h ({percent_str} %)"+" "*( max_num_chars_project_name-num_chars_hours )+"-┘"])
+                    table_rows.append([f"└-{total_hours_row_str} h ({percent_str} %)"+" "*(max_num_chars_project_name-num_chars_hours)+"-┘"])
 
             # Print the table:
             print(f"Week nº{n_week} ({TOTAL_WORKING_HOURS_EACH_WEEK[n_week]} working hours):")
@@ -227,9 +234,10 @@ if __name__ == "__main__":
     # Visualize the weeks graphically:
     if input("Generate graph? [y/N] ").lower().startswith("y"):
         import matplotlib.pyplot as plt
+        import matplotlib.ticker as mtick
         import json
 
-        projects             = list(hours_per_project.keys())
+        projects             = sort_projects(hours_per_project, COLORS_CONFIG_FILE)
         project_data         = [
             [hours_per_project[project][date]['hours'] for date in contemplated_dates] for project in projects
         ]
@@ -239,26 +247,29 @@ if __name__ == "__main__":
         ]
         contemplated_mondays = [date for date in contemplated_dates if date.weekday() == 0]
         bottom               = [0] * len(contemplated_mondays)
-
         colors               = load_colors_config(COLORS_CONFIG_FILE)
-        plt.figure(figsize=(20, 12))
 
-        #TODO: sort projects
+        plt.figure(figsize=(25, 6))
         for p in range(len(projects)):
             plt.bar(contemplated_mondays, project_data_percent[p],
-                    width=3, bottom=bottom, label=projects[p], color=colors.get(projects[p])
+                    width=5, bottom=bottom, label=projects[p], color=colors.get(projects[p]),
+                    edgecolor='black' if projects[p] == 'VACACIONES (23-0004)' else None, linewidth=0.2 if projects[p] == 'VACACIONES (23-0004)' else None
             )
-            # Stack the bars so that each week adds up to 100%:
+            # Stack the bars so each week sums up to 100%:
             bottom = [bottom[s] + project_data_percent[p][s] for s in range(len(contemplated_mondays))]
-
-        plt.ylim([0, 100])
-        plt.legend()
+        plt.legend(bbox_to_anchor=(-0.15, 1.0), loc='upper left')
         plt.xlabel("Date")
+        plt.grid(axis='y', which='major')
+        plt.minorticks_on()
+        plt.grid(axis='y', which='minor', linewidth='0.8', linestyle=':')
+        plt.ylim([0, 100])
+        plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
         plt.ylabel("% Hours")
+        plt.tight_layout(pad=2.5)
         first_week_num, first_week_year = contemplated_dates[0].isocalendar()[1],  contemplated_dates[0].isocalendar()[0]
         last_week_num,  last_week_year  = contemplated_dates[-1].isocalendar()[1], contemplated_dates[-1].isocalendar()[0]
         plt.title(f"Worked hours per project between week #{first_week_num} of {first_week_year} and week #{last_week_num} of {last_week_year}")
 
         filename = f"TAW - Worked hours per project from {contemplated_dates[0]} to {contemplated_dates[-1]}.png"
         plt.savefig(filename)
-        print(f'Graph saved in the file: "{filename}"')
+        print(f'Graph saved in file: "{filename}"')
